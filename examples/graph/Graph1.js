0b10cfe99a0fd8e4998bfac9bc353590ca71abbc
banli <867889876@qq.com>
2023-08-23 23:03:57 +0800

    update

--- examples/graph/Graph1.js
+++ examples/graph/Graph1.js
@@ -39,6 +39,46 @@ class Graph {
     }
     return -1
   }
+
+  bfs() {
+    const queue = [this.vertices[0]]
+    const visited = {}
+
+    while (queue.length) {
+      const cur = queue.shift()
+      console.log(`访问到了`, cur)
+
+      visited[cur.index] = true
+      // 查找所有的邻接点
+      for (let i = 0; i < this.edges[cur.index].length; i++) {
+        if (this.edges[cur.index][i] === 1 && !visited[i]) {
+          queue.push(this.vertices[i])
+        }
+      }
+    }
+
+    this.vertices.forEach((v) => {
+      if (!visited[v.index]) {
+        console.log(`没有访问到的点`, v)
+      }
+    })
+  }
+
+  dfs() {
+    const visited = {}
+    const _dfs = (v) => {
+      console.log(`访问到了`, v)
+      visited[v.index] = true
+
+      for (let i = 0; i < this.edges[v.index].length; i++) {
+        if (this.edges[v.index][i] === 1 && !visited[i]) {
+          _dfs(this.vertices[i])
+        }
+      }
+    }
+
+    _dfs(this.vertices[0])
+  }
 }
 
 class Vertex {
@@ -58,5 +98,9 @@ arr.forEach((v) => g1.addVertex(v))
 
 // 添加边
 g1.addEdge('a', 'c') //  这里增加的是顶点
+g1.addEdge('c', 'b')
 g1.addEdge('c', 'd')
 console.log(g1)
+
+// g1.bfs()
+g1.dfs()
